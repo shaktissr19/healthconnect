40f35ef5978f1a5767a949992865d9d03c32bb45
shaktissr19 <shakti.ssr19@gmail.com>
2026-08-16 15:43:33 +0530

    Preserve Health Score history export with partial scoring engine

--- healthconnect-api/src/services/healthScore.service.ts
+++ healthconnect-api/src/services/healthScore.service.ts
@@ -1,1 +1,2 @@
-export * from './healthScore.v2_2.service';
+export { HEALTH_SCORE_ALGORITHM_VERSION, getHealthScoreHistory } from './healthScore.v2_1.service';
+export { calculateHealthScore, getHealthScore, refreshHealthScore } from './healthScore.v2_2.service';
