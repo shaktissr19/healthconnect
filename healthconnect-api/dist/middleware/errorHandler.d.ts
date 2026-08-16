d7abf0a1c76e7188864171e91ea09c2a2c799630
shaktissr19 <shakti.ssr19@gmail.com>
2026-08-16 18:10:14 +0530

    chore(repo): stop tracking generated API artifacts

--- healthconnect-api/dist/middleware/errorHandler.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const errorHandler: (error: Error, req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>>;
-//# sourceMappingURL=errorHandler.d.ts.map
